be845db4d53b94158ba86327b2641a143b27b91a
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2023-04-28 11:19:59 +0100

    wip: add dynamic form

--- src/app/pages/prison-break/prison-break.component.ts
+++ src/app/pages/prison-break/prison-break.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-prison-break',
@@ -7,7 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrisonBreakComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  matrix: number[][] = [];
+  matrixCount  = 0;
+  escapePath = 0;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      rows: this.fb.array([
+
+        this.fb.array([
+        this.fb.control('')
+        ])
+
+
+
+      ])
+    });
+  }
+  get rows() {
+    return this.form.get('rows') as FormArray;
+  }
+
+  getRowArray(index: number) {
+    return this.form.get('rows')?.get(index +'') as FormArray;
+  }
+
+  addCells(){
+    this.addRow()
+    this.addColumn()
+  }
+
+
+  addRow() {
+
+const colNums = this.getRowArray(0).length
+
+    this.rows.push(this.fb.array(Array.from({ length: colNums }, () => this.fb.control(''))));
+
+  }
+
+  addColumn(): void {
+ // const rows =  this.rows.
+
+ this.rows.controls.forEach(row  => {
+    (row as FormArray).push( this.fb.control(''))
+ });
+
+
+  }
+
+  onSubmit(): void {
+    this.matrix = this.form.value.rows;
+    this.matrixCount = this.matrix.length;
+    this.escapePath = this.calculateEscapePath();
+  }
+  calculateEscapePath(): number {
+
+    return 1
+  }
+
+
 
   ngOnInit(): void {
   }
