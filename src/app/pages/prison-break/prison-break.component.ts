8e7b292616294c5612605f32c7174b30e330a075
Toyeeb-Abdulrahmon <131885912+Toyeeb-Abdulrahmon@users.noreply.github.com>
2023-04-28 16:31:22 +0100

    wip: add escape path algorithm

--- src/app/pages/prison-break/prison-break.component.ts
+++ src/app/pages/prison-break/prison-break.component.ts
@@ -1,81 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
-
 @Component({
   selector: 'app-prison-break',
   templateUrl: './prison-break.component.html',
-  styleUrls: ['./prison-break.component.scss']
+  styleUrls: ['./prison-break.component.scss'],
 })
-export class PrisonBreakComponent implements OnInit {
-
+export class PrisonBreakComponent {
   form!: FormGroup;
-  matrix: number[][] = [];
-  matrixCount  = 0;
   escapePath = 0;
 
   constructor(private fb: FormBuilder) {
-   this.initialiseForm()
+    this.initialiseForm();
   }
+
   initialiseForm() {
-
     this.form = this.fb.group({
-      rows: this.fb.array([
-
-        this.fb.array([
-        this.fb.control('')
-        ])
-
-
-
-      ])
+      rows: this.fb.array([this.fb.array([this.fb.control('')])]),
     });
   }
+  onSubmit(): void {
+    this.escapePath = this.calculateEscapePath(this.form.value.rows);
+  }
+
+  calculateEscapePath(matrix: any): number {
+    console.log(matrix);
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+    const dp = Array.from({ length: numRows + 1 }, () =>
+      Array(numCols + 1).fill(0)
+    );
+    dp[0][1] = 1;
+    for (let row = 1; row <= numRows; row++) {
+      for (let col = 1; col <= numCols; col++) {
+        if (parseInt(matrix[row - 1][col - 1]) === 0) {
+          dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+        }
+      }
+    }
+    return dp[numRows][numCols];
+  }
+
   get rows() {
     return this.form.get('rows') as FormArray;
   }
+  addCells() {
+    this.addRow();
+    this.addColumn();
+  }
 
   getRowArray(index: number) {
-    return this.form.get('rows')?.get(index +'') as FormArray;
+    return this.rows.get(index + '') as FormArray;
   }
-
-  addCells(){
-    this.addRow()
-    this.addColumn()
-  }
-
 
   addRow() {
+    const colNums = this.getRowArray(0).length;
 
-const colNums = this.getRowArray(0).length
-
-    this.rows.push(this.fb.array(Array.from({ length: colNums }, () => this.fb.control(''))));
-
+    this.rows.push(
+      this.fb.array(Array.from({ length: colNums }, () => this.fb.control('')))
+    );
   }
 
   addColumn(): void {
- // const rows =  this.rows.
-
- this.rows.controls.forEach(row  => {
-    (row as FormArray).push( this.fb.control(''))
- });
-
-
+    this.rows.controls.forEach((row) => {
+      (row as FormArray).push(this.fb.control(''));
+    });
   }
-
-  onSubmit(): void {
-    this.matrix = this.form.value.rows;
-    this.matrixCount = this.matrix.length;
-    this.escapePath = this.calculateEscapePath();
-  }
-  calculateEscapePath(): number {
-
-    return 1
-  }
-
-
-
-  ngOnInit(): void {
-  }
-
 }
